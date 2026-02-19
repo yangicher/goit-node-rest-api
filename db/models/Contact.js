02594a0a84a9fdc4b4369c007261a8bc9c221c10
Serge Iangicher <sergey.yangicher@rockyroad.io>
2026-02-19 20:31:56 +0200

    add models

--- db/models/Contact.js
+++ db/models/Contact.js
@@ -19,6 +19,10 @@ const Contact = sequelize.define(
       type: DataTypes.BOOLEAN,
       defaultValue: false,
     },
+    owner: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
   }
 );
 
---
+++ db/models/Users.js
@@ -0,0 +1,28 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../sequelize.js";
+
+const Users = sequelize.define(
+  'users', {
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    subscription: {
+        type: DataTypes.ENUM,
+        values: ["starter", "pro", "business"],
+        defaultValue: "starter"
+    },
+    token: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+    },
+  }
+);
+
+export default Users;
+
